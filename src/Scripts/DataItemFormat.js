a12032d77df22e524d454195baa259f88f5cb5c6
altry104 <altry104@gmail.com>
2022-09-11 12:08:03 +0300

    fix2

--- src/Scripts/DataItemFormat.js
+++ src/Scripts/DataItemFormat.js
@@ -1,10 +1,6 @@
-<<<<<<< HEAD
 import {GetRandomElement} from "./Helper.js"
 
 export class DataItem{
-=======
-export class DataItem {
->>>>>>> 430dfd10e4ca942a535d27405021ac47ea924ffc
     id="UIWEFIB34RVB89CG23EHW"
     city="Харків"
     index="61110"
@@ -22,7 +18,6 @@ export class DataItem {
     hospital = "Закрито"                   //open //close // none
     hospitalDistance = "0.8"            //km
     photo = [
-<<<<<<< HEAD
         "https://static.espreso.tv/uploads/photobank/248000_249000/248536_Harkiv_reuters_new_960x380_0.jpg",
         "https://static.ukrinform.com/photos/2022_04/thumb_files/630_360_1651221771-528.jpg",
         "https://media.slovoidilo.ua/media/publications/17/162467/ilyustratyvne-foto_large.jpg",
@@ -31,16 +26,6 @@ export class DataItem {
         "https://images.unian.net/photos/2022_03/thumb_files/400_0_1647337339-3790.jpg?0.25597438004291195",
         "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZ1aaieEdsZCxNybPekghGXNp8QYNVQPGznloHqGx6OVAp4yzZdSu6H-ZOflJCrrfFWQ4&usqp=CAU",
         "https://media.slovoidilo.ua/media/publications/16/152935/152935-1_large.jpg",
-=======
-        "https://vgorode.ua/img/article/12054/66_main-v1651827046.jpg",
-        "https://static.ukrinform.com/photos/2022_03/thumb_files/630_360_1648291469-236.jpg",
-        "https://static.ukrinform.com/photos/2022_03/thumb_files/630_360_1647409818-915.jpg",
-        "https://s.mind.ua/img/forall/a/202390/1.jpg?1648883127",
-        "https://resizer.finance.ua/aafa4f1b0b73911d0d6caba266929369022a4ff9.jpg?url=https://finance-news-media.fra1.cdn.digitaloceanspaces.com/prod/9/a/9a46e4ea14bcffa7ba02765ba242dfa3&fit=cover&w=382.5&h=",
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR4J9XY-Tv2rQhruCeuBMWxUa-qT61m2id6qQ&usqp=CAU",
-        "https://kor.ill.in.ua/m/610x385/2747773.jpg",
-        "https://static.espreso.tv/uploads/photobank/248000_249000/248536_Harkiv_reuters_new_960x380_0.jpg",
->>>>>>> 430dfd10e4ca942a535d27405021ac47ea924ffc
     ]
 }
 
